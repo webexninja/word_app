ce724034a3ccb43fb1670a35c13cf5312475e032
webexninja <shivanktiware@gmail.com>
2015-12-13 20:50:10 +0530

    Add controller with index action

--- app/controllers/pair_controller.rb
+++ app/controllers/pair_controller.rb
@@ -1,4 +1,7 @@
 class PairController < ApplicationController
   def home
   end
+
+  def index
+  end
 end
--- test/controllers/pair_controller_test.rb
+++ test/controllers/pair_controller_test.rb
@@ -6,4 +6,9 @@ class PairControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
 end
